edc3b7325a7fcac26069960c93dfaa97a3233f82
AlvaroYanez01 <alvaroy479@gmail.com>
2021-03-29 17:24:25 -0500

    guardar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular001';
+ diaSemana:string  = "Martes 2 de Marzo del 2021";
+ 
+ nombre:string = "Alvaro";
+ apellido:string ="Yanez";
+
+  /*-----------------------------------------------------------------*/
+  cadenaTexto: string ='Ingrese Datos';
+
+  rutaImagen:string="http://assets.stickpng.com/images/5847ea22cef1014c0b5e4833.png";
+
+  /*CREAR UN CONSTRUCTOR */
+  activado =true;
+
+//Event Binding
+
+
+textoEventBinding: string ="Este es un ejemplo de Event Binding"
+
+
+//two way binding
+
+cadenaTwoWayBinding: string ='FISEI';
+
+  constructor(){
+    setInterval(()=> (this.activado=false), 3000)
+  }
+
+
+ sumar(numero:number,numero2:number){
+
+  return numero+numero2;
+
+ }
+
+ mostrarMensaje(){
+   return this.cadenaTexto;
+ }
+
+cambiarTexto(): void{
+this.textoEventBinding = "EL texto ha cambiado esta es la prueba";
 }
+
+}
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
